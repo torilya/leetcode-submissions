ffc2d1b96c8d004834b693add0d585c63af24bb3
Ilya Toritsyn <ilya.toritsyn@gmail.com>
2024-11-21 15:29:20 +0000

    [LeetCode Sync] Runtime - 12 ms (48.99%), Memory - 9.8 MB (28.97%)

--- submissions/0789-kth-largest-element-in-a-stream/solution.go
+++ submissions/0789-kth-largest-element-in-a-stream/solution.go
@@ -1,39 +1,46 @@
-import (
-    "fmt"
-	"slices"
-)
+import "container/heap"
+
+type minHeap []int
+
+func (mh minHeap) Len() int           { return len(mh) }
+func (mh minHeap) Less(i, j int) bool { return mh[i] < mh[j] }
+func (mh minHeap) Swap(i, j int)      { mh[i], mh[j] = mh[j], mh[i] }
+
+func (mh *minHeap) Push(x any) { *mh = append(*mh, x.(int)) }
+
+func (mh *minHeap) Pop() any {
+	old := *mh
+	n := len(old)
+	*mh = old[0 : n-1]
+	return old[n-1]
+}
 
 type KthLargest struct {
 	k    int
-	nums []int
+	nums *minHeap
 }
 
 func Constructor(k int, nums []int) KthLargest {
+	mh := &minHeap{}
+    heap.Init(mh)
+	kl := KthLargest{k, mh}
 
-	if k > len(nums)+1 {
-		panic("k > nums.length")
+	for _, num := range nums {
+		kl.Add(num)
 	}
-
-    kl := KthLargest{k: k}
-
-	for _, n := range nums {
-        kl.Add(n)
-    }
 
 	return kl
 }
 
 func (this *KthLargest) Add(val int) int {
-    if len(this.nums) < this.k || val > this.nums[0] {
-        i, _ := slices.BinarySearch(this.nums, val)
-        this.nums = slices.Insert(this.nums, i, val)
+	if this.nums.Len() < this.k {
+		heap.Push(this.nums, val)
+	} else if val > (*this.nums)[0] {
+		heap.Pop(this.nums)
+		heap.Push(this.nums, val)
+	}
 
-        if len(this.nums) > this.k {
-            this.nums = this.nums[1:]
-        }
-    }
-
-	return this.nums[0]
+	return (*this.nums)[0]
 }
 
 /**
