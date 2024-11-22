df8ecf729480b8ed9b0b54374c7694b9916dcdc3
Ilya Toritsyn <ilya.toritsyn@gmail.com>
2024-11-22 16:08:20 +0000

    [LeetCode Sync] Runtime - 5 ms (89.47%), Memory - 10.8 MB (14.29%)

--- submissions/0789-kth-largest-element-in-a-stream/solution.go
+++ submissions/0789-kth-largest-element-in-a-stream/solution.go
@@ -1,28 +1,61 @@
-import "container/heap"
-
 type minHeap []int
 
-func (mh minHeap) Len() int           { return len(mh) }
-func (mh minHeap) Less(i, j int) bool { return mh[i] < mh[j] }
-func (mh minHeap) Swap(i, j int)      { mh[i], mh[j] = mh[j], mh[i] }
+func (mh minHeap) heapify(nodeIndex int) {
+	if nodeIndex <= 0 {
+		return
+	}
 
-func (mh *minHeap) Push(x any) { *mh = append(*mh, x.(int)) }
+	parentIndex := (nodeIndex - 1) / 2
 
-func (mh *minHeap) Pop() any {
-	old := *mh
-	n := len(old)
-	*mh = old[0 : n-1]
-	return old[n-1]
+	if mh[nodeIndex] < mh[parentIndex] {
+		mh[nodeIndex], mh[parentIndex] = mh[parentIndex], mh[nodeIndex]
+		mh.heapify(parentIndex)
+	}
+}
+
+func (mh minHeap) push(x int) minHeap {
+	mhNew := append(mh, x)
+	mhNew.heapify(len(mhNew) - 1)
+
+	return mhNew
+}
+
+func (mh minHeap) heapifyTopDown(nodeIndex int) {
+	childLeftIndex := nodeIndex*2 + 1
+	childRightIndex := childLeftIndex + 1
+
+	mhLength := len(mh)
+
+	minIndex := nodeIndex
+
+	if childLeftIndex < mhLength && mh[childLeftIndex] < mh[minIndex] {
+		minIndex = childLeftIndex
+	}
+	if childRightIndex < mhLength && mh[childRightIndex] < mh[minIndex] {
+		minIndex = childRightIndex
+	}
+
+	if nodeIndex != minIndex {
+		mh[nodeIndex], mh[minIndex] = mh[minIndex], mh[nodeIndex]
+		mh.heapifyTopDown(minIndex)
+	}
+}
+
+func (mh minHeap) pop() minHeap {
+	mh[0] = mh[len(mh)-1]
+	mhNew := mh[:len(mh)-1]
+	mhNew.heapifyTopDown(0)
+
+	return mhNew
 }
 
 type KthLargest struct {
 	k    int
-	nums *minHeap
+	nums minHeap
 }
 
 func Constructor(k int, nums []int) KthLargest {
-	mh := &minHeap{}
-    heap.Init(mh)
+	var mh minHeap
 	kl := KthLargest{k, mh}
 
 	for _, num := range nums {
@@ -33,14 +66,13 @@ func Constructor(k int, nums []int) KthLargest {
 }
 
 func (this *KthLargest) Add(val int) int {
-	if this.nums.Len() < this.k {
-		heap.Push(this.nums, val)
-	} else if val > (*this.nums)[0] {
-		heap.Pop(this.nums)
-		heap.Push(this.nums, val)
+	this.nums = this.nums.push(val)
+
+	if len(this.nums) > this.k {
+		this.nums = this.nums.pop()
 	}
 
-	return (*this.nums)[0]
+	return this.nums[0]
 }
 
 /**
