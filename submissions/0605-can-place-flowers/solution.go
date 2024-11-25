204e3b92a58ca63822dcd190f866b037aea637f9
Ilya Toritsyn <ilya.toritsyn@gmail.com>
2024-11-25 06:46:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.1 MB (18.42%)

--- submissions/0605-can-place-flowers/solution.go
+++ submissions/0605-can-place-flowers/solution.go
@@ -3,14 +3,18 @@ func canPlaceFlowers(flowerbed []int, n int) bool {
         return true
     }
     
-    for i, plot := range flowerbed {
-        if plot == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == len(flowerbed)-1 || flowerbed[i+1] == 0) {
+    flowerbedLen := len(flowerbed)
+
+    for i := 0; i < flowerbedLen; i++ {
+        if flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == flowerbedLen-1 || flowerbed[i+1] == 0) {
             flowerbed[i] = 1
             n--
 
             if n <= 0 {
                 return true
             }
+
+            i++
         }
     }
 
